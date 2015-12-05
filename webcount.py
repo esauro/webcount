5ecc2e6dbf2c297cd21ed5c5057ceed6d36cea52
Esau Rodriguez <esauro@gmail.com>
2015-12-05 15:01:38 +0000

    Fixing error code when none url was sent

--- webcount.py
+++ webcount.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask import request
+from flask import abort
 from parser.webtokenizer import BSTokenizer
 import json
+
 
 app = Flask(__name__)
 
 
 @app.route('/api/', methods=["POST"])
 def counter():
-    tokenizer = BSTokenizer(request.form["url"])
-
+    if (request.form["site"] is None) or (request.form["site"] == ""):
+        return abort(400)
+    tokenizer = BSTokenizer(request.form["site"])
     return json.dumps(tokenizer.get_most_common_words(100))
 
 
