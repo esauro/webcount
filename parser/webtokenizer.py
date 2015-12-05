a43a033dca3e60b97f6efa298fb6e34bdf8716f2
Esau Rodriguez <esauro@gmail.com>
2015-12-05 15:00:46 +0000

    Typo and some minor tweaks

--- parser/webtokenizer.py
+++ parser/webtokenizer.py
@@ -8,10 +8,10 @@ class BSTokenizer:
     def __init__(self, url):
         self.raw_content = urllib2.urlopen(url).read()
         self.soup = BeautifulSoup(self.raw_content, "html.parser")
-        garbage = [s.extract() for s in self.soup(['style', 'script', '[document]', 'head', 'title', 'meta'])]
+        [s.extract() for s in self.soup(['style', 'script', '[document]', 'head', 'title', 'meta'])]
 
     def get_most_common_words(self, amount):
         text = self.soup.getText()
-        freqs = Counter(text.split())
-        return dict(freqs.most_common(amount))
+        frequencies = Counter(text.split())
+        return frequencies.most_common(amount)
 
