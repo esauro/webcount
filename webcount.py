296f26a9e40f7cf6ba14ccfda9b6d16fb57c0e18
Esau Rodriguez <esauro@gmail.com>
2015-12-05 11:16:39 +0000

    First version of API and frontend without tag cloug

--- webcount.py
+++ webcount.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask import request
 from parser.webtokenizer import BSTokenizer
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    tokenizer = BSTokenizer("https://bbc.co.uk")
-    return repr(tokenizer.get_most_common_words(10))
+@app.route('/api/', methods=["POST"])
+def counter():
+    tokenizer = BSTokenizer(request.form["url"])
+
+    return json.dumps(tokenizer.get_most_common_words(100))
 
 
 if __name__ == '__main__':
